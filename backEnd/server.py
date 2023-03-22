b669cf646cad715c22698623c5b6d92b8c6b22bc
Randi <dev@dochot.be>
2023-03-22 15:24:21 +0100

    python flask example of database

--- backEnd/server.py
+++ backEnd/server.py
@@ -3,6 +3,7 @@ from flask import Flask, request
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
@@ -37,6 +38,54 @@ class AbstractResource(Resource):
         return "", 200
 
 
+# All model :
+class adminModel(db.Model):
+    """
+    admin model (person who can log in to the system)
+    """
+    __tablename__ = "admin"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+    def __repr__(self):
+        return "<admin %r>" % self.name
+
+
+# define all resource :
+class addAdmin(AbstractResource):
+    def post(self):
+        arguments = request.get_json()
+        pwd = arguments.get("password")
+        del arguments["password"]
+        pwd = generate_password_hash(pwd)
+
+        admin = adminModel(**arguments, password=pwd)
+        adminModel.query.session.add(admin)
+        db.session.commit()
+
+        return "", 201
+
+
+class getAdmin(Resource):
+    def get(self):
+        admin = db.session.query(adminModel).filter_by(name="admin").first()
+        rtn = admin.as_dict()
+        del rtn["password"]  # remove password from the response
+        return rtn, 200
+
+
+# All resource (API) :
+
+
+api.add_resource(addAdmin, "/admin-add")
+
+
+# Main :
 @app.route("/")
 def hello():
     return "Test web site for the Flask API"
