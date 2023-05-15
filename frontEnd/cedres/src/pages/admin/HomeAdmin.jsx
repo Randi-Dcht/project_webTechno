7d29679f6b8bc8738c962af77513bd316eb3bd2e
Randi <dev@dochot.be>
2023-05-15 08:50:14 +0200

    add link in home page of admin

--- frontEnd/cedres/src/pages/admin/HomeAdmin.jsx
+++ frontEnd/cedres/src/pages/admin/HomeAdmin.jsx
@@ -1,8 +1,41 @@
+import {Button, Card, CardGroup} from "react-bootstrap";
+
+const listLink =[
+    {
+        name : "Etudiants",
+        link: "",
+        text: "Liste des étudiants et consulter les profiles"
+    },
+    {
+        name : "Demandes",
+        link: "",
+        text: "Liste des demandes (accepter, modifier, consulter)"
+    },
+]
+
 const HomeAdmin = () =>
 {
     return(
         <div>
-            <h1>Accueil administration</h1>
+            <h2 className="m-4">Accueil administration</h2>
+            <CardGroup>
+                {
+                    listLink.map((e, index)=>
+                    {
+                        return(
+                            <Card key={index} className='m-2'>
+                                <Card.Body>
+                                    <Card.Title>{e.name}</Card.Title>
+                                    <Card.Text>{e.text}</Card.Text>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <Button  variant='warning'>consulter</Button>
+                                </Card.Footer>
+                            </Card>
+                        )
+                    })
+                }
+            </CardGroup>
         </div>
     )
 }
